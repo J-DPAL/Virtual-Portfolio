251ecc8f3366600045902c9b0d1245b1b70f6306
Jean-David Pallares <jeandavid.pallares@gmail.com>
2026-01-20 14:10:37 -0500

    Setup i18n with English and Arabic translations

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import en from './en.json';
-import fr from './fr.json';
+import en from './locales/en.json';
+import fr from './locales/fr.json';
 
 i18n
   .use(initReactI18next)
