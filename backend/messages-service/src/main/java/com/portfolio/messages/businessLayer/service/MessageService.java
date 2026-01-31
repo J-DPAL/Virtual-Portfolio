227c916db2fb48a39463ff4dd69e3469649a2b95
Jean-David Pallares <jeandavid.pallares@gmail.com>
2026-01-31 12:17:33 -0500

    Update messages service security

--- backend/messages-service/src/main/java/com/portfolio/messages/businessLayer/service/MessageService.java
+++ backend/messages-service/src/main/java/com/portfolio/messages/businessLayer/service/MessageService.java
@@ -14,6 +14,7 @@ import com.portfolio.messages.utils.exceptions.ResourceNotFoundException;
 
 @Service
 @Transactional
+@SuppressWarnings("null")
 public class MessageService {
 
   private final MessageRepository messageRepository;
